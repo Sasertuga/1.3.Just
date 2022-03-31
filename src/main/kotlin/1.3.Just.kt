596cc6fda4d718bf55ef83656304b01915ff1096
Yarygin Maksim <goprobeaheroo@vk.com>
2022-03-31 23:21:57 +0500

    add project 1.3.Just

---
+++ src/main/kotlin/1.3.Just.kt
@@ -0,0 +1,33 @@
+const val MINUTE = 60
+const val HOUR = 3600
+const val DAY = 86400
+
+fun main() {
+    agoToText(59)
+}
+
+fun agoToText(timeToAgo: Int) {
+    val toMinute = timeToAgo / MINUTE
+    val toHour = timeToAgo / HOUR
+
+    val correctnessForMinute = when {
+        toMinute % 10 == 1 -> "минуту"
+        toMinute % 10 == 2 || toMinute % 10 == 3 || toMinute % 10 == 4 -> "минуты"
+        else -> "минут"
+    }
+
+    val correctnessForHour = when {
+        toHour % 10 == 1 -> "час"
+        toHour % 10 == 2 || toHour % 10 == 3 || toHour % 10 == 4 -> "часа"
+        else -> "часов"
+    }
+
+    when {
+        timeToAgo < MINUTE -> println("был(а) только что")
+        timeToAgo in MINUTE until HOUR -> println("был(а) $toMinute $correctnessForMinute назад")
+        timeToAgo in HOUR until DAY -> println("был(а) $toHour  $correctnessForHour назад")
+        timeToAgo >= DAY && timeToAgo < DAY * 2 -> println("был(а) сегодня")
+        timeToAgo >= DAY * 2 && timeToAgo < DAY * 3 -> println("был(а) вчера")
+        timeToAgo >= DAY * 3 -> println("был(а) давно")
+    }
+}
