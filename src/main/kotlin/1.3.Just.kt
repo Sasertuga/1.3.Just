2ffe625d10ff81976b94dd62a51772d1638172cd
Yarygin Maksim <goprobeaheroo@vk.com>
2022-04-01 23:44:24 +0500

    revision val correctnessForMinute

--- src/main/kotlin/1.3.Just.kt
+++ src/main/kotlin/1.3.Just.kt
@@ -3,7 +3,7 @@ const val HOUR = 3600
 const val DAY = 86400
 
 fun main() {
-    agoToText(59)
+    agoToText(56478)
 }
 
 fun agoToText(timeToAgo: Int) {
@@ -11,7 +11,8 @@ fun agoToText(timeToAgo: Int) {
     val toHour = timeToAgo / HOUR
 
     val correctnessForMinute = when {
-        toMinute % 10 == 1 -> "минуту"
+        toMinute % 10 == 1 && toMinute != 11 -> "минуту"
+        toMinute == 12 || toMinute == 13 || toMinute == 14 -> "минут"
         toMinute % 10 == 2 || toMinute % 10 == 3 || toMinute % 10 == 4 -> "минуты"
         else -> "минут"
     }
@@ -25,7 +26,7 @@ fun agoToText(timeToAgo: Int) {
     when {
         timeToAgo < MINUTE -> println("был(а) только что")
         timeToAgo in MINUTE until HOUR -> println("был(а) $toMinute $correctnessForMinute назад")
-        timeToAgo in HOUR until DAY -> println("был(а) $toHour  $correctnessForHour назад")
+        timeToAgo in HOUR until DAY -> println("был(а) $toHour $correctnessForHour назад")
         timeToAgo >= DAY && timeToAgo < DAY * 2 -> println("был(а) сегодня")
         timeToAgo >= DAY * 2 && timeToAgo < DAY * 3 -> println("был(а) вчера")
         timeToAgo >= DAY * 3 -> println("был(а) давно")
